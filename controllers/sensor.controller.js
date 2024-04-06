4b6dc14855b18992bce6b0c459df21d6730346dd
ZarcoOscarDSM31 <al222210607@gmail.com>
2024-04-06 10:29:33 -0600

    con la bd

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -1,4 +1,61 @@
 import Sensor from '../models/sensor.model.js';
+import { MongoClient } from 'mongodb';
+import fetch from 'node-fetch';
+
+const url = 'mongodb://localhost:27017/';
+const dbName = 'Aplicaction';
+const collectionName = 'sensores';
+const sensorIP = 'http://192.168.100.41/datos';
+
+async function fetchDataFromSensor() {
+    try {
+        const response = await fetch(sensorIP);
+        const sensorData = await response.json();
+        return sensorData;
+    } catch (error) {
+        console.error('Error al obtener datos del sensor:', error);
+        return null;
+    }
+}
+
+async function insertDataIntoMongoDB(data) {
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Agregar el timestamp actual al objeto de datos
+        const timestampedData = {
+            ...data,
+            timestamp: new Date() // Agrega el timestamp actual
+        };
+
+        const result = await collection.insertOne(timestampedData);
+        console.log(`Se insertaron ${result.insertedCount} documentos en MongoDB`);
+
+    } catch (error) {
+        console.error('Error al insertar datos en MongoDB:', error);
+    } finally {
+        await client.close();
+    }
+}
+
+
+async function main() {
+    const sensorData = await fetchDataFromSensor();
+    if (sensorData) {
+        await insertDataIntoMongoDB(sensorData);
+    }
+}
+
+// Llamar a la función main() inicialmente
+main();
+
+// Programar la ejecución de la función main() cada 15 minutos
+setInterval(main, 900000); // 15 minutos * 60 segundos/minuto * 1000 milisegun
 
 export const getAllSensors = async (req, res) => {
     try {
@@ -13,7 +70,7 @@ export const createSensor = async (req, res) => {
     const sensor = new Sensor(req.body);
     try {
         const newSensor = await sensor.save();
-        res.status(201).json({ message: 'Sensor creado correctamente', sensor: newSensor});
+        res.status(201).json({ message: 'Sensor creado correctamente', sensor: newSensor });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -37,7 +94,7 @@ export const updateSensor = async (req, res) => {
         if (!sensor) {
             return res.status(404).json({ message: 'Sensor no encontrado' });
         }
-        res.status(200).json({ message: 'Sensor actualizado correctamente', sensor});
+        res.status(200).json({ message: 'Sensor actualizado correctamente', sensor });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
