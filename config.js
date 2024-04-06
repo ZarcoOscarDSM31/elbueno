d923f73ab96ff7eb82be0ef07719178f55ed0c15
ZarcoOscarDSM31 <al222210607@gmail.com>
2024-04-06 14:46:59 -0600

    base de datos en la nube

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = "mongodb://localhost:27017/Aplication";
+export const MONGODB_URI = "mongodb+srv://oscarzarco:l73FwuphtkarC9nI@cluster0.tqythyg.mongodb.net/";
 export const TOKEN_SECRET = "secret";
 export const FRONTEND_URL = "http://localhost:5000";
