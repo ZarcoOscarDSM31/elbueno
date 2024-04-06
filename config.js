75884efdeec47caf17f2bec33b1801f0f8027dbf
ZarcoOscarDSM31 <al222210607@gmail.com>
2024-04-06 14:07:26 -0600

    arreglé la conexión a la bd

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = "mongodb://localhost/Aplication";
+export const MONGODB_URI = "mongodb://localhost:27017/Aplication";
 export const TOKEN_SECRET = "secret";
 export const FRONTEND_URL = "http://localhost:5000";
